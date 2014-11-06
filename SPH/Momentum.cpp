d53c4af409da9f13146395f9785f86cd4f2effdb
Sam Hunter <Sam@Sams-MacBook-Pro.local>
2014-11-06 22:18:11 +0000

    fixed stuff for george

--- SPH/Momentum.cpp
+++ SPH/Momentum.cpp
@@ -8,13 +8,42 @@
 
 #include "Momentum.h"
 
-void findAccel(std::vector<Particle*> plist, infoModule* module)
+std::array<double,3> acceleration (int i,std::vector<Particle*> plist, infoModule* module)
 {
+    std::array<double, 3> sum;
     double r;
     double dkernel;
-    std::array<double, 3> sum;
     double dxdr;
     double p_i, p_j, rho_i, rho_j;
+    
+    p_i = plist[i]->pressure[0];
+    rho_i = plist[i]->density[0];
+    
+    for (int j = 0; j<plist[i]->neighbours.size(); j++)
+    {
+        r = plist[i]->neighboursdist[j];
+        dkernel = wendkernel(r,  module);
+        p_j = plist[i]->neighbours[j]->pressure[0];
+        rho_j = plist[i]->neighbours[j]->density[0];
+        
+        for (int Dim = 0; Dim < module->nDim; Dim++)
+        {
+            dxdr = (plist[i]->position[Dim] - plist[i]->neighbours[j]-> position[Dim])/r;
+            //momentum eqn
+            sum[Dim] = sum[Dim] - (plist[i]->neighbours[j]->m[0]*(p_j/pow(rho_j, 2) + p_i/pow(rho_i, 2)) * dxdr * dkernel);
+        }
+        
+        
+    }
+    return sum;
+
+}
+
+void findAccel(std::vector<Particle*> plist, infoModule* module)
+{
+    
+    std::array<double, 3> dvdt;
+    
     
     for (int i = 0; i<plist.size(); i++)
     {
@@ -22,39 +51,18 @@ void findAccel(std::vector<Particle*> plist, infoModule* module)
         {
             continue;
         }
-        sum[0] = 0.0;
-        sum[1] = 0.0;
-        sum[2] = 0.0;
-        p_i = plist[i]->pressure[0];
-        rho_i = plist[i]->density[0];
-        for (int j = 0; j<plist[i]->neighbours.size(); j++)
-        {
-            r = plist[i]->neighboursdist[j];
-            dkernel = wendkernel(r,  module);
-            p_j = plist[i]->neighbours[j]->pressure[0];
-            rho_j = plist[i]->neighbours[j]->density[0];
-            
-            for (int Dim = 0; Dim < module->nDim; Dim++)
-            {
-                dxdr = (plist[i]->position[Dim] - plist[i]->neighbours[j]-> position[Dim])/r;
-                //momentum eqn
-                sum[Dim] =  - (plist[i]->neighbours[j]->m[0]*(p_j/pow(rho_j, 2) + p_i/pow(rho_i, 2)) * dxdr * dkernel);
-                
-                // gravity acts in y in 2d and in z in 3d
-                if (Dim == module->nDim -1)
-                {
-                    sum[Dim] = sum[Dim] - 9.81;
-                }
-            }
-            
-            
-        }
+        dvdt[0] = 0.0;
+        dvdt[1] = 0.0;
+        dvdt[2] = 0.0;
         
-        for (int Dim = 0; Dim < module->nDim; Dim++)
-        {
-            plist[i]->prevAccel[Dim] = plist[i]->accel[Dim];
-            plist[i]->accel[Dim] = sum[Dim];
-        }
+        dvdt = acceleration(i, plist, module);
+        
+        // gravity acts in y in 2d and in z in 3d
+        dvdt[module->nDim -1] = dvdt[module->nDim -1] - 9.81;
+    
+        
+        plist[i]->prevAccel = plist[i]->accel;
+        plist[i]->accel = dvdt;
         
     }
     
@@ -62,11 +70,8 @@ void findAccel(std::vector<Particle*> plist, infoModule* module)
 
 void findNextAccel(std::vector<Particle*> plist, infoModule* module)
 {
-    double r;
-    double dkernel;
-    std::array<double, 3> sum;
-    double dxdr;
-    double p_i, p_j, rho_i, rho_j;
+    std::array<double, 3> dvdt;
+    
     
     for (int i = 0; i<plist.size(); i++)
     {
@@ -74,40 +79,17 @@ void findNextAccel(std::vector<Particle*> plist, infoModule* module)
         {
             continue;
         }
-        sum[0] = 0.0;
-        sum[1] = 0.0;
-        sum[2] = 0.0;
-        p_i = plist[i]->pressure[0];
-        rho_i = plist[i]->density[0];
-        for (int j = 0; j<plist[i]->neighbours.size(); j++)
-        {
-            r = plist[i]->neighboursdist[j];
-            dkernel = wendkernel(r,  module);
-            p_j = plist[i]->neighbours[j]->pressure[0];
-            rho_j = plist[i]->neighbours[j]->density[0];
-            
-            for (int Dim = 0; Dim < module->nDim; Dim++)
-            {
-                dxdr = (plist[i]->position[Dim] - plist[i]->neighbours[j]-> position[Dim])/r;
-                //momentum eqn
-                sum[Dim] = -1*(plist[i]->neighbours[j]->m[0]*(p_j/pow(rho_j, 2) + p_i/pow(rho_i, 2)) * dxdr * dkernel);
-                
-                // gravity?
-                if (Dim  == module->nDim -1)
-                {
-                    sum[Dim] = sum[Dim] - 9.81;
-                }
-                
-            }
-            
-            
-        }
+        dvdt[0] = 0.0;
+        dvdt[1] = 0.0;
+        dvdt[2] = 0.0;
         
-        for (int Dim = 0; Dim < module->nDim; Dim++)
-        {
+        dvdt = acceleration(i, plist, module);
+        
+        // gravity acts in y in 2d and in z in 3d
+        dvdt[module->nDim -1] = dvdt[module->nDim -1] - 9.81;
             
-            plist[i]->nextAccel[Dim] = sum[Dim];
-        }
+        plist[i]->nextAccel = dvdt;
+        
         
     }
     
